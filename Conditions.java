6e2ea5ea58b29d2f2614a01c9c231ad7e6187ae1
khushiy05 <khushiyewale150@gmail.com>
2026-04-22 16:19:13 +0530

    for practice

---
+++ Conditions.java
@@ -0,0 +1,59 @@
+/*class FirstClass {
+        public static void main(String args[]) {
+            // this is how we find output in Java 
+        System.out.print("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!\n");
+        System.out.println("Hello, World!\n\ni love java");
+
+
+
+    /* print the pattern  
+    *
+    **
+    ***
+    ****
+    */
+   /*System.out.println("*");
+   System.out.println("**");
+   System.out.println("***");
+   System.out.println("****");
+
+   /* variables in java+dsa
+   int a = 10;
+   int b = 25;
+   int sum = a + b;
+   //System.out.println(sum);
+   //int diff = a - b;
+   int mul = a * b;
+   System.out.println(mul);*/ 
+    //}
+//}
+
+/*import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //input
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        /*
+        nextInt()
+        nextInt() */
+        //System.out.println(name);
+    //}
+//}
+
+/*import java.util.*;
+public class Conditions {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if(age >= 18) {
+            System.out.println("Adult");
+        } else {
+            System.out.println("Not Adult");
+        }
+    }      
+}
+    */
